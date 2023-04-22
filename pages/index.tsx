705ff5199585f5e1dece218ff7f54c568e6fc2b3
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-22 14:35:20 -0600

    Built header and a follow bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import Header from '@/components/Header';
 
 export default function Home() {
-  return <div className='text-3xl text-sky-500'>Hello World!</div>;
+  return (
+    <>
+      <Header label='Home' />
+    </>
+  );
 }
