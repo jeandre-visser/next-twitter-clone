29ffa040d96c20f25e7381b6c9bb04bb8fd0c3d2
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-22 16:55:25 -0600

    Prisma client and authentication

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,43 @@
+import bcrypt from 'bcrypt';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import prisma from '../../../libs/prismadb';
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error('Credentials not found');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid password');
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
