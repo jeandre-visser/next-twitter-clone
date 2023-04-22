6e199fd6f683930147813ddca90ff5176a2af551
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-22 15:03:13 -0600

    Built modal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
+import Modal from '@/components/Modal';
 import Layout from '@/components/layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Modal isOpen title='Test Modal' actionLabel='Submit' />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
