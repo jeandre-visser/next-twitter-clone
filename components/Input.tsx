a6b4172fd120a503c24bbede2f8db2f83c44e002
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-22 15:06:56 -0600

    Built input component

---
+++ components/Input.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+  type?: string;
+  disabled?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  value,
+  type,
+  disabled,
+  onChange,
+}) => {
+  return (
+    <input
+      disabled={disabled}
+      type={type}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      className='w-full p-4 text-lg bg-balack border-2 border-neutral-800 rounded-md outline-none text-white focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:cursor-not-allowed disabled:opacity-70'
+    />
+  );
+};
+
+export default Input;
