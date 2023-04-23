c1344e7b6f947f978dd86287174684d40c62552d
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-23 15:50:47 -0600

    Change head title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Flitter</title>
+        <meta
+          name='description'
+          content='A social media app built with Next.js'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
