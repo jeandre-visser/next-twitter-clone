7a43127ae90643b9237fe067a502ba6550ea6eb6
Jeandre Visser <jeandre@nutrimeals.ca>
2023-04-23 15:37:54 -0600

    Screenshots

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -61,7 +61,7 @@ const LoginModal = () => {
   const footerContent = (
     <div className='text-neutral-400 text-center mt-4'>
       <p>
-        First time using Twitter?
+        First time using Flitter?
         <span
           onClick={onToggle}
           className='
